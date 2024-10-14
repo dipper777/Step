a6ee6a00222c0c08d23cb54753df0b19be5caef3
dipper777 <116436656+dipper777@users.noreply.github.com>
2024-10-14 21:46:50 +0300

    Add files via upload

---
+++ stepdz_8/main.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+#Це повідомлення відладки
+logging.debug('')
+#інформаційне повідомлення
+logging.info('')
+#попередження
+logging.warning('')
+#помилка
+logging.error('')
+#критична помилка
+logging.critical('')
